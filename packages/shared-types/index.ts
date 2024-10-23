556773f6a88c24f54f23b160f1d3f35c5d643f39
Weston C. Beecroft <westoncb@gmail.com>
2024-10-23 16:19:33 -0700

    use latest sonnet 3.5 checkpoint

--- packages/shared-types/index.ts
+++ packages/shared-types/index.ts
@@ -18,7 +18,7 @@ export interface InferenceResult {
 }
 
 export const AnthropicModels = [
-  'claude-3-5-sonnet-20240620',
+  'claude-3-5-sonnet-20241022',
   'claude-3-opus-20240229',
   'claude-3-haiku-20240307',
   'claude-3-sonnet-20240229'
